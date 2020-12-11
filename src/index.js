caead1308e71cca70d681bff045574b25754c3bd
Luan Nascimento <luan.nascimento11@outlook.com>
2020-12-10 23:03:14 -0300

    HTTP Methods

--- src/index.js
+++ src/index.js
@@ -2,11 +2,38 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({message: 'Hello World'});
-})
-
-app.listen(3333, () => {
-  console.log('Backend started! 🎈')
+app.get('/projects', (request, response) => {
+  return response.json([
+    'Projeto 1',
+    'Projeto 2'
+  ]);
 });
-//aa
+
+app.post('projects', (request, response) => {
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+    'Projeto 3'
+  ]);
+});
+
+
+app.put('projects/:id', (request, response) => {
+  return response.json([
+    'Projeto 3',
+    'Projeto 2',
+    'Projeto 3'
+  ]);
+});
+
+
+app.delete('projects/:id', (request, response) => {
+  return response.json([
+    'Projeto 3',
+    'Projeto 3'
+  ]);
+});
+app.listen(3333, () => {
+  console.log('Backend started! (°-°)/')
+});
+
